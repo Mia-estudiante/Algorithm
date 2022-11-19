2160dd209981ffcc3b049c82079d539a277b8c62
mia-estudiante <90076@naver.com>
2022-11-19 16:47:49 +0900

    Implementation-Problem2: Solved

---
+++ implementation2-me.py
@@ -0,0 +1,9 @@
+H = int(input())
+count = 0
+
+for h in range(H+1):
+    for m in range(60):
+        for s in range(60):
+            if '3' in str(h) or '3' in str(m) or '3' in str(s):
+                count += 1
+print(count)
