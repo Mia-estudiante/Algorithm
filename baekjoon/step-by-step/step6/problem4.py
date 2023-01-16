7ab71e34552ff636d5641e9b910b80d898e70ba1
mia-estudiante <90076@naver.com>
2023-01-16 15:39:57 +0900

    Baekjoon-Step by Step-2675: modify

--- baekjoon/step-by-step/step6/problem4.py
+++ baekjoon/step-by-step/step6/problem4.py
@@ -3,6 +3,6 @@ print = sys.stdout.write
 
 for _ in range(int(input())):
     rs = list(input().split())
-    for w in list(rs[1]):
+    for w in rs[1]:
         print(w*int(rs[0]))
     print('\n')
