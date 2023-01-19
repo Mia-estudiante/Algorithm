a550f6c904f400f15572a318f1ab2cf333c2b4cd
mia-estudiante <90076@naver.com>
2023-01-19 22:21:06 +0900

    Baekjoon-Step by Step-2908: Best

--- baekjoon/step-by-step/step6/problem7-1.py
+++ baekjoon/step-by-step/step6/problem7-1.py
@@ -1,5 +1,4 @@
 def numbers(n):
-    n = int(n) # 추가
     result = ''
     while n>0:
         a=n%10
@@ -8,5 +7,10 @@ def numbers(n):
     return int(result)
 
 a, b = map(numbers, input().split())
-# a, b= numbers(a), numbers(b)
+a, b= numbers(a), numbers(b)
 print(a) if a>b else print(b)
+
+'''
+map 함수 내 함수를 넣든 안 넣든, 
+일단 문자열을 다루는 것보다 "숫자"를 다루는 것이 속도 면에서 좋다
+'''
